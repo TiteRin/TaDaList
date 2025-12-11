7b8683fe0caf39ebf3540e61c9b6b37293e520dc
Marine Lancien <marine.lancien@protonmail.com>
2025-12-11 11:02:27 +0100

    feat: add random congratulatory messages to TodayTasksCounter
    - Introduce a function for generating random congratulatory messages.
    - Update task count display to include dynamic congratulatory text.
    - Improve user engagement with motivational messaging.

--- src/app/components/tasks/TodayTasksCounter.tsx
+++ src/app/components/tasks/TodayTasksCounter.tsx
@@ -2,12 +2,45 @@ interface TodayTasksCounterProps {
     count: number
 }
 
+const CONGRATULATIONS_MSG = [
+    "Congratulations!",
+    "You did it!",
+    "You're awesome!",
+    "Awesome!",
+    "Amazing!",
+    "You’re the best!",
+    "Bravo",
+    "You rock!",
+    "Way to go!",
+    "Well done!",
+    "Good job!",
+    "You nailed it!",
+    "You crushed it!",
+    "Fantastic!",
+    "Brilliant!",
+    "Incredible",
+    "Impressive!",
+    "Outstanding!",
+    "You’re on fire!",
+    "Congrats!",
+    "Hurray!",
+    "Wow!",
+    "Excellent!",
+    "Remarkable!"
+];
+
+function getRandomCongratulation() {
+    return CONGRATULATIONS_MSG[Math.floor(Math.random() * CONGRATULATIONS_MSG.length)];
+}
+
 export default function TodayTasksCounter({count}: TodayTasksCounterProps) {
 
-    return (<span className="today-tasks-counter w-full text-center text-xl bg-info/40 rounded-sm px-4 py-2"
+    const congrats = count > 0 && getRandomCongratulation();
+
+    return (<span className="today-tasks-counter"
                   data-testid="today-tasks-counter" >
         {count === 0 && "No tasks done today… yet!"}
-        {count === 1 && "1 task done today!"}
-        {count > 1 && `${count} tasks done today!`}
+        {count === 1 && `${congrats} 1 task done today!`}
+        {count > 1 && `${congrats} ${count} tasks done today!`}
     </span>)
 }
